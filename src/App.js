011769663e3097531474e0c3e75f8ddefa61af79
Gaurav Ghai <gaurav.ghai@buildingblocks.in-at-176047117876>
2020-05-19 14:10:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,14 @@ function App() {
         <a href="https://www.htmlhints.com/article/how-to-create-toggle-switch/93">
           Download Source Code
         </a>{" "}
-        | Developed by <a href="https://www.gauravghai.dev/">Gaurav Ghai</a>
+        | Developed by{" "}
+        <a target="_blank" href="https://www.gauravghai.dev/">
+          Gaurav Ghai
+        </a>{" "}
+        | Powered by{" "}
+        <a target="_blank" href="https://www.htmlhints.com/">
+          HTML HINTS
+        </a>
       </div>
     </React.Fragment>
   );
